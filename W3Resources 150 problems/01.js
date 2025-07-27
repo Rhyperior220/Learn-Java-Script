a9b00e20e543124b6a7a909bdf3544fa692a359d
Arjun Kale <shreekale1080@gmail.com>
2025-07-27 12:18:18 +0000

    Solved 1st problem

--- W3Resources 150 problems/01.js
+++ W3Resources 150 problems/01.js
@@ -1,8 +1,27 @@
 // 1. Display Current Day and Time
 
 // Write a JavaScript program to display the current day and time in the following format.  
-// Sample Output : Today is : Tuesday.
+// Sample Output : 
+// Today is : Tuesday.
 // Current time is : 10 PM : 30 : 38
 
-console.log("Hello me")
-console.log("Hello Arjun")
+// Day Thing
+let today = new Date()
+let myDay = today.getDay().toLocaleString('en-IN')
+let Daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+// Hour Thing
+let hour = today.getHours() + 5
+if (hour > 12) {
+    hour = hour - 12;
+}
+// Minute Thing
+let min = today.getMinutes() +30
+if (min > 60){
+    min = min - 60;
+}
+//Second Thing
+let sec = today.getSeconds()
+
+console.log(`Today is: ${Daylist[myDay]}.`)
+console.log(`Current Time is : ${hour} PM : ${min} : ${sec} `)
