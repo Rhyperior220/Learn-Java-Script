f89bb94b0aef156feaf44c382967586b37261a9e
Arjun Kale <shreekale1080@gmail.com>
2025-07-27 15:32:40 +0530

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,7 @@
+// 1. Display Current Day and Time
+
+// Write a JavaScript program to display the current day and time in the following format.  
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+console.log("Hello me")
